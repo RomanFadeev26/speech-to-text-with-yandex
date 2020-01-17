d816ea8ba6c8a8ba861cc9c0f918be1a55c48f31
romanfadeev <4pybNV!!>
2020-01-17 17:23:44 +0300

    Move keys to ENV

--- server/auth/consts.js
+++ server/auth/consts.js
@@ -1,5 +1,9 @@
-const FOLDER_ID = 'b1g7fguhq2u0ackp3cnh';
+const FOLDER_ID = process.env.FOLDER_ID;
+const KEY_ID = process.env.KEI_ID;
+const ISS = process.env.ISS;
 
 module.exports = {
-    FOLDER_ID
+    FOLDER_ID,
+    KEY_ID,
+    ISS
 };
--- server/auth/jwt.js
+++ server/auth/jwt.js
@@ -1,22 +1,20 @@
 const jose = require('node-jose');
 const fs = require('fs');
+const { KEY_ID, ISS } = require('./consts');
 
 const privateKey = fs.readFileSync(require.resolve('../keys/private.pem'));
-
-const keyId = 'ajef6v4vsibimde7ulkm';
-
 
 async function createJWT() {
     const now = Math.floor((new Date().getTime()) / 1000);
 
     const payload = {
-        iss: 'ajer2lqufoaf562j8ltv',
+        iss: ISS,
         aud: 'https://iam.api.cloud.yandex.net/iam/v1/tokens',
         iat: now,
         exp: now + 3600
     };
 
-    const sign = await jose.JWK.asKey(privateKey, 'pem', { kid: keyId, alg: 'PS256' });
+    const sign = await jose.JWK.asKey(privateKey, 'pem', { kid: KEY_ID, alg: 'PS256' });
     return jose.JWS.createSign({ format: 'compact' }, sign)
         .update(JSON.stringify(payload))
         .final();
